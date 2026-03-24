31b559f063205fe965d57111608e01438070697a
pacoito123 <pacoito@protonmail.com>
2026-03-24 16:40:04 -0600

    Renamed MultipleCondition and RangeCondition to be like the other abstract Conditions. - Also moved RangeOperator validation to a static function within itself.

--- loaforcsSoundAPI/SoundPacks/Conditions/.RangeCondition.cs
+++ loaforcsSoundAPI/SoundPacks/Conditions/.RangeCondition.cs
@@ -20,6 +20,66 @@ public struct RangeOperator<T>(T min, T max) where T : struct, IComparable<T> {
     public override readonly string ToString() {
         return $"{min}..{max}";
     }
+
+    /// <summary>
+    /// Validates a range operator's formatting.
+    /// </summary>
+    /// <param name="condition">Value to attempt to parse into a range operator.</param>
+    /// <param name="range">Valid range operator to use for this Condition.</param>
+    /// <param name="result">Unsuccessful validations, if any were found.</param>
+    /// <param name="tryParse">TODO.</param>
+    /// <param name="defaultRange">TODO.</param>
+    /// <returns>Whether the validation succeeded or not.</returns>
+    public static bool ValidateRangeOperator(string condition, out RangeOperator<T> range, out IValidatable.ValidationResult result, ParseAction tryParse, RangeOperator<T> defaultRange = default) {
+        range = defaultRange;
+        result = null!;
+        if(string.IsNullOrEmpty(condition)) {
+            result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Range operator can not be missing or empty!");
+            return false;
+        }
+
+        string[] parts = condition.Split("..", StringSplitOptions.None);
+
+        switch(parts.Length) {
+            case 1:
+                // Case when there's only one number in the condition.
+                T target = range.min;
+                if(!tryParse(parts[0], ref target)) {
+                    // Invalid input.
+                    result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Failed to parse: '{parts[0]}' as a '{typeof(T).FullName}'!");
+                    break;
+                }
+                range = new(target);
+                break;
+            case 2:
+                // Case when there's a range specified.
+                T lowerBound = range.min;
+                if(!tryParse(parts[0], ref lowerBound)) {
+                    // Invalid input.
+                    result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Failed to parse: '{parts[0]}' as a '{typeof(T).FullName}'!");
+                    break;
+                }
+
+                T upperBound = range.max;
+                if(!tryParse(parts[1], ref upperBound)) {
+                    // Invalid input.
+                    result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Failed to parse: '{parts[1]}' as a '{typeof(T).FullName}'!");
+                    break;
+                }
+
+                range = new(lowerBound, upperBound);
+                break;
+            case > 2:
+                result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Range operator '{condition}' uses '..' more than once!");
+                break;
+            default:
+                break;
+        }
+
+        return result == null;
+    }
+
+    public delegate bool ParseAction(string value, ref T result);
 }
 
 public abstract class RangeCondition<T> : Condition where T : struct, IComparable<T> {
@@ -65,65 +125,14 @@ public abstract class RangeCondition<T> : Condition where T : struct, IComparabl
         return condition.EvaluateRangeOperator(value);
     }
 
-    /// <inheritdoc cref="ValidateRangeOperator(string,out RangeOperator&lt;T&gt;,out IValidatable.ValidationResult)"/>
-    protected bool ValidateRangeOperator(string condition, out IValidatable.ValidationResult result) {
-        return ValidateRangeOperator(condition, out _range, out result);
-    }
-
     /// <summary>
     /// Validates a range operator's formatting.
     /// </summary>
     /// <param name="condition">Value to attempt to parse into a range operator.</param>
-    /// <param name="range">Valid range operator to use for this Condition.</param>
     /// <param name="result">Unsuccessful validations, if any were found.</param>
     /// <returns>Whether the validation succeeded or not.</returns>
-    protected bool ValidateRangeOperator(string condition, out RangeOperator<T> range, out IValidatable.ValidationResult result) {
-        range = DefaultRange;
-        result = null!;
-        if(string.IsNullOrEmpty(condition)) {
-            result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Range operator can not be missing or empty!");
-            return false;
-        }
-
-        string[] parts = condition.Split("..", StringSplitOptions.None);
-
-        switch(parts.Length) {
-            case 1:
-                // Case when there's only one number in the condition.
-                T target = range.min;
-                if(!TryParseValue(parts[0], ref target)) {
-                    // Invalid input.
-                    result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Failed to parse: '{parts[0]}' as a '{typeof(T).FullName}'!");
-                    break;
-                }
-                range = new(target);
-                break;
-            case 2:
-                // Case when there's a range specified.
-                T lowerBound = range.min;
-                if(!TryParseValue(parts[0], ref lowerBound)) {
-                    // Invalid input.
-                    result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Failed to parse: '{parts[0]}' as a '{typeof(T).FullName}'!");
-                    break;
-                }
-
-                T upperBound = range.max;
-                if(!TryParseValue(parts[1], ref upperBound)) {
-                    // Invalid input.
-                    result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Failed to parse: '{parts[1]}' as a '{typeof(T).FullName}'!");
-                    break;
-                }
-
-                range = new(lowerBound, upperBound);
-                break;
-            case > 2:
-                result = new IValidatable.ValidationResult(IValidatable.ResultType.FAIL, $"Range operator '{condition}' uses '..' more than once!");
-                break;
-            default:
-                break;
-        }
-
-        return result == null;
+    protected bool ValidateRangeOperator(string condition, out IValidatable.ValidationResult result) {
+        return RangeOperator<T>.ValidateRangeOperator(condition, out _range, out result, TryParseValue, DefaultRange);
     }
 
     /// <summary>
